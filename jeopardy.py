c3aeff7928b29675003f4b9ba79ddb9623ae04a4
Jonathan Bateman <jondeveloper0@gmail.com>
2022-12-28 00:00:19 -0500

    I am creating a dictionary that will hold all of the categories and the number of questions of that type of category

--- jeopardy.py
+++ jeopardy.py
@@ -1,26 +1,22 @@
 import csv
 import random
+import re
 
+def num_questions(filename):
+    """
+    This function counts how many jeopardy questions there are in this file.
+    """
 
-def counter_line(filename):
-    counter = 0
-    with open(filename,encoding="utf-8") as file:
-
-        csv_file = csv.reader(file)
-
-        next(csv_file)
-
-        for line in csv_file:
-            counter += 1
-    
-    return counter
-
+    with open(filename, encoding="utf-8") as file:
+        questions = file.readlines()
+        num = len(questions)
+        return num
 
 def get_random_question(filename, category):
 
     counter = 0 
     quesetion = []
-    categories = []
+    category_dict = {} # this dict will store the number of each type of question in it along with the name of the category
     with open(filename,encoding="utf-8") as file:
 
         csv_file = csv.reader(file)
@@ -28,6 +24,11 @@ def get_random_question(filename, category):
         next(csv_file)
 
         for line in csv_file:
+            line[3] = category
+            if category not in category_dict:
+                category_dict[category] = 1
+            else:
+                category_dict[category] += 1
             
             categories.append(line[3])
             # I want to go to every row where the category matches.
@@ -44,7 +45,7 @@ def get_random_question(filename, category):
     return category, get_random_question
 
 def main():
-
+    print(num_questions("JEOPARDY_CSV.csv"))
     print(get_random_question("JEOPARDY_CSV.csv", "HISTORY"))
 
 
