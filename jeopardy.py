ccca4bc33c79e40274bc0fd7fb5493b0f749f39e
Jonathan Bateman <jondeveloper0@gmail.com>
2022-12-28 01:28:37 -0500

    I am currently trying to undestand how Google's API works so I can give it a question to answer

--- jeopardy.py
+++ jeopardy.py
@@ -1,6 +1,7 @@
 import csv
 import random
 import re
+from serpapi import GoogleSearch
 
 def num_questions(filename):
     """
@@ -11,12 +12,10 @@ def num_questions(filename):
         questions = file.readlines()
         num = len(questions)
         return num
+def get_random_question(filename):
 
-def get_random_question(filename, category):
-
-    counter = 0 
-    quesetion = []
     category_dict = {} # this dict will store the number of each type of question in it along with the name of the category
+    question_answer_dict = {}
     with open(filename,encoding="utf-8") as file:
 
         csv_file = csv.reader(file)
@@ -24,30 +23,44 @@ def get_random_question(filename, category):
         next(csv_file)
 
         for line in csv_file:
-            line[3] = category
+            category = line[3]
+            question = line[5]
+            answer = line[6]
+            if question not in question_answer_dict:
+                question_answer_dict[question] = answer
+
             if category not in category_dict:
                 category_dict[category] = 1
             else:
                 category_dict[category] += 1
-            
-            categories.append(line[3])
-            # I want to go to every row where the category matches.
-            # Then I want to get a random question that matches that category.
-            if category == line[3]:
-                counter += 1
-                quesetion.append(line[5])
 
-    random_question = random.randint(0,counter)
-    get_random_question = quesetion[random_question]
+    question1, value = random.choice(list(question_answer_dict.items()))
 
-    #lets go
-    
-    return category, get_random_question
+    return question1 # this is a question
+
+
+def search_answer(qwery,api_key):
+
+    params = {
+    "api_key": api_key, # this is my api key
+    "engine": "google",
+    "q": qwery,
+    "location": "United States",
+    "google_domain": "google.com",
+    "gl": "us",
+    "hl": "en"
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_json()
+
+    return results
 
 def main():
-    print(num_questions("JEOPARDY_CSV.csv"))
-    print(get_random_question("JEOPARDY_CSV.csv", "HISTORY"))
-
-
+    question = get_random_question("JEOPardy_CSV.csv")
+    print(question)
+    # answer = search_answer(question,"41096232330069c7f458ec7fe95a39f301220053fba1af409772c3ceea5d9fa0")
+    # print(answer)
+    
 if __name__ == "__main__":
     main()
